c8ade3117c6d236295d4dc61c744bcd92f2b1df3
Gonzalo-Montilla <montillalondono@gmail.com>
2025-05-23 21:04:01 -0500

    Add files via upload

---
+++ Listas.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+
+public class Main {
+    public static void main(String[] args) {
+
+        list<String>estudiantes= new ArrayList<String>();
+
+        estudiantes.add("mauricio");
+        estudiantes.add("Gonzo");
+        estudiantes.add("Chalo");
+
+        System.out.println(estudiantes.getFirst);
+
+    }
+}
+
