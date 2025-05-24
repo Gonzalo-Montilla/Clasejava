6363a3a8355aa5997c1d38916f0a1039677db256
Gonzalo-Montilla <montillalondono@gmail.com>
2025-05-23 21:04:41 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Tipos de datos);
+
+                int entero=3;
+                long enteroMaslargo=99999999;
+                double puntoFlotante=999999.0;
+                float puntoFlotantePeque=9.4f;
+                char letra='a';
+                boolean esBoleano=true;
+                
+
+
+    }
+}
